73384f81afcbe2b0b45ef20c9d57be2d229e0e1d
Ryan Ball <ryanball@Ryans-MacBook-Pro.local>
2016-08-07 00:36:15 +0100

    Initial Commit

---
+++ The Front Page/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  The Front Page
+//
+//  Created by Ryan Ball on 07/08/2016.
+//  Copyright © 2016 Ryan Ball. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
